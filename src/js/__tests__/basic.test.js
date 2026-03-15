80aae24c391185b82c7d71349304570673222a77
LKl <plat.elena2010@ya.ru>
2026-03-15 14:10:51 +0300

    New Jest test added

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -13,9 +13,21 @@ test("no matching props", () => {
 });
 
 
+test("function returns the right first object", () => {
+  const obj = {name: "мечник", health: 10, level: 2, attack: 80, defence: 40};
+  const result = orderByProps(obj, ["name", "level"]);
+  expect(result[0]).toStrictEqual({key: "name", value: "мечник"});
+});
+
 
 test("function returns what expected", () => {
   const obj = {name: "мечник", health: 10, level: 2, attack: 80, defence: 40};
   const result = orderByProps(obj, ["name", "level"]);
-  expect(result[0]).toStrictEqual({key: "name", value: "мечник"});
+  expect(result).toStrictEqual([
+    {key: "name", value: "мечник"}, 
+    {key: "level", value: 2}, 
+    {key: "attack", value: 80}, 
+    {key: "defence", value: 40}, 
+    {key: "health", value: 10}
+  ]);
 });
